9ec79e05ce71fc76ba23005b366dec955686f425
renanjp <renanjohannsen@hotmail.com>
2019-05-22 16:18:07 -0300

    Adição da página web para controlar a lampada

---
+++ wevsite-lampada/precache-manifest.6a643b6bb509b7d7a0c985e5471cb8cc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d6f76f123272f5cd3fff788d74d83a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c66acb12f2f99c9c405c",
+    "url": "/static/css/main.c6a3abb9.chunk.css"
+  },
+  {
+    "revision": "67d8ec57211ac1f92379",
+    "url": "/static/js/2.2065807e.chunk.js"
+  },
+  {
+    "revision": "c66acb12f2f99c9c405c",
+    "url": "/static/js/main.ee226121.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
